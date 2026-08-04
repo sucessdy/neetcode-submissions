76e81e1401974f281d4e8fea812716f39b6e82dc
Muktā <109413908+sucessdy@users.noreply.github.com>
2026-08-04 23:08:39 +0530

    Add: two-integer-sum - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum/submission-6.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+ 
+map<int, int> mpp ; 
+
+
+for( int i = 0; i < nums.size() ; i++) { 
+    int diff = target - nums[i];
+    
+    if (mpp.contains(diff)) { 
+
+
+return  {mpp[diff], i }  ;
+     }
+    mpp[nums[i]] = i;
+}
+return {} ; 
+    }};
